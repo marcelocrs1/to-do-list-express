8732cf9606e2039d19e21ba5c272d312cf471f2c
Marcelo Santos <msantos@bimms.net>
2022-08-04 13:33:34 +0100

    Adiantando resposta json

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const { application } = require("express");
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>My ToDo List: OK</h1>");
+});
+
+app.get("/json", (req, res) => {
+  res.json({ title: "Tarefa X", done: true });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor foi Iniciado!");
+});
